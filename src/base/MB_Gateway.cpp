cdb153ef2cedebbc0a7fd37f85d862493c9ce7b1
icke2063 <icke@X230iSSD>
2013-09-29 19:04:13 +0200

    add DummyFonctor usage example (commented)

--- src/base/MB_Gateway.cpp
+++ src/base/MB_Gateway.cpp
@@ -20,6 +20,8 @@ using namespace std;
 using namespace icke2063::MB_Framework;
 
 #include <SummerySlave.h>
+#include <DummyFunctor.h>
+
 #ifdef I2C_SUPPORT
 	#include <I2CScanner.h>
 #endif
@@ -42,6 +44,18 @@ int8_t offset =1;
 	scanner.reset(new icke2063::MB_Gateway::I2C::I2C_Scanner());
 
 	while (1) {
+//		max_functor = 30;
+//		cout << "add:" << (int)max_functor  << endl;
+//		for(int i=0;i<max_functor;i++){
+//			pool->addFunctor(new Dummy_Functor());
+//		}
+//
+//
+//		max_functor = max_functor + (offset);
+//		offset *= 2;
+//
+//		if((max_functor + offset) > 100)offset = -1;
+//		if(max_functor < 0){offset = 1;max_functor = 1;}
 
 
 		sleep(1);
